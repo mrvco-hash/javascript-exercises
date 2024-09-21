3df2f4fe7d92b991012f90dbe2d0dfe81305d464
mrvco-hash <marcandykouadio@gmail.com>
2024-09-21 09:17:53 +0000

    Proposition de solution exo 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    output = '';
+    i = 0;
+    if (num < 0) {
+        output = 'ERROR'
+    } else {
+        while (i < num) {
+            output += str;
+            ++i
+        }
+    }
+    return output;
 };
 
 // Do not edit below this line
