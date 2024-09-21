9b098c5fa3661ede7c2c7441b969460d6acd6be5
mrvco-hash <marcandykouadio@gmail.com>
2024-09-21 09:33:19 +0000

    Proposition de solution exo 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    strList = str.split("");
+    strList = strList.reverse();
+    strList = strList.join("");
 
+    return strList;
 };
 
 // Do not edit below this line
