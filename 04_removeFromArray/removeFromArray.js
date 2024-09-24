556f568e8ff051550e8588fac4e7cb2a7f1dd0cd
mrvco-hash <marcandykouadio@gmail.com>
2024-09-24 12:01:04 +0000

    Proposition de solution pour l'exo 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(liste, ...delListe) {
+    for (let num of delListe) {
+        for (i = 0; i < liste.length; i++) {
+            while (liste[i] === num) {
+                liste.splice(liste.indexOf(liste[i]), 1);
+            }
+        }
+    }
+    return liste;
 };
 
 // Do not edit below this line
