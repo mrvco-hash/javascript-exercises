d01918bbb2aba874d1d4061ecbafaa9d1835d8e4
mrvco-hash <marcandykouadio@gmail.com>
2024-09-24 12:46:26 +0000

    Proposition de solution de l'exo 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,31 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let total = 0;
+    let compteur = 0;
 
+    if ((Number.isInteger(num1)) && Number (Number.isInteger(num2))) {
+        if ((num1 < 0) || (num2 < 0)) {
+            return "ERROR";
+        } else {
+            if (num1 < num2) {
+                while (num1 <= num2) {
+                    total += num1
+            
+                    num1++
+                    compteur++
+                }
+            } else if (num1 > num2) {
+                while (num2 <= num1) {
+                    total += num2
+            
+                    num2++
+                    compteur++
+                }
+            }
+            return total
+        }
+    } else {
+        return "ERROR"
+    }
 };
 
 // Do not edit below this line
