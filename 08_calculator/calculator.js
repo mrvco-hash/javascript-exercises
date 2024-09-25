dd89b574c2979c672c8176f5ed72b048b4cc883e
mrvco-hash <marcandykouadio@gmail.com>
2024-09-25 15:47:54 +0000

    Proposition de solution pour l'exo 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,50 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let somme = 0;
+  let i = 0;
+  if (array.length === 0) {
+    return somme;
+  } else {
+    for (i = 0; i < array.length; i++) {
+      somme += array[i];
+    }
+    return somme;
+  }
+
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let result = 1;
+  for (i = 0; i < array.length; i++) {
+    result *= array[i]
+  }
+  return result;
 };
 
-const power = function() {
-	
+const power = function(num, power) {
+	return num ** power;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num === 0) {
+    return 1;
+  } else if (num > 0) {
+    let i = 1;
+    let fact = 1;
+    while (i <= num) {
+
+      fact *= i
+      ++i
+    }
+    return fact;
+  }
 };
 
 // Do not edit below this line
