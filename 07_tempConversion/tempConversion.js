39136499a924861281f88b5fb469cd17c9521a22
mrvco-hash <marcandykouadio@gmail.com>
2024-09-25 10:20:17 +0000

    Proposition de solution pour l'exo 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,16 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let tempConvertie = (temp - 32) * (5/9);
+  let tempConvertieArrondie = parseFloat(tempConvertie.toFixed(1))
+  if (tempConvertieArrondie === 0.0) {
+    tempConvertieArrondie = 0
+  }
+  return tempConvertieArrondie;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let tempConvertie = (temp * (9/5)) + 32
+  let tempConvertieArrondie = parseFloat(tempConvertie.toFixed(1))
+  return tempConvertieArrondie;
 };
 
 // Do not edit below this line
