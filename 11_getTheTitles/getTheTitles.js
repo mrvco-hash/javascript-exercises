40d97ccadd268a111792db4583085a2967465e83
mrvco-hash <marcandykouadio@gmail.com>
2024-10-15 12:50:06 +0000

    Added a solution for getTheTitles exercices

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,10 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(array) {
+    const booksTitles = [];
+    for (let i = 0; i < array.length; i++) {
+        aBook = array[i];
+        booksTitles.push(aBook.title);
+    }
+    return booksTitles;
 };
 
 // Do not edit below this line
