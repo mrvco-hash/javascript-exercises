ffe8e1f86caef0d3be1800baa98405e5f31b5f1f
mrvco-hash <marcandykouadio@gmail.com>
2024-10-15 12:35:47 +0000

    Adding a solution for the Fibonacci sequence exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+    const fibonacciList = [0, 1];
+    if (typeof number == "string") {
+        number = Number(number)
+    }
+    if (number < 0) {
+        return "OOPS";
+    } else {
+        for (let i = 0; i < number; i++) {
+            let fibonacciListFirst = fibonacciList[fibonacciList.length - 1];
+            let fibonacciListSecond = fibonacciList[fibonacciList.length - 2];
+            fibonacciList.push(fibonacciListFirst + fibonacciListSecond);
+        }
+        return fibonacciList[fibonacciList.length - 2];
+    }
 };
 
 // Do not edit below this line
